bbef2b1a3b1f97b225ea7bb15e7fc55f441b57a3
Coma-cell <jayprataps36@gmail.com>
2025-03-29 22:01:00 +0530

    #2
    ceiling lamp

--- Scenes/arms.gd
+++ Scenes/arms.gd
@@ -3,9 +3,9 @@ var walking = false
 var blend_amount := 0.0
 @export var zoom_in : StringName = "zoom_in"
 @export var zoom_out : StringName = "zoom_out"
-@export var spotlight : StringName = "Shoulder_light"
+@export var spotlight : StringName = "flash_light"
 @export var angle_step : float = 3
-@export var power_step : float = 3
+@export var power_step : float = 18
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,12 +16,12 @@ func _process(delta: float) -> void:
 	walking = Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")
 
 	if walking:
-		var target_blend := 1.0
+		var target_blend := 1.7
 		if Input.is_action_pressed("sprint"):
-			target_blend = 2.0
+			target_blend = 3.0
 		blend_amount = lerp(blend_amount, target_blend, 0.1)  # Smoothly transition to target blend amount
 	else:
-		blend_amount = lerp(blend_amount, 0.0, 0.1)  # Smoothly transition to idle
+		blend_amount = lerp(blend_amount, 0.0, 0.2)  # Smoothly transition to idle
 
 	$Skeleton3D/AnimationTree.set("parameters/idle_to_walk/blend_amount", blend_amount)
 	if Input.is_action_just_pressed("zoom_in"):
@@ -32,11 +32,11 @@ func _process(delta: float) -> void:
 		adjust_spotlight_power(-power_step)
 
 func adjust_spotlight_angle(step: float) -> void:
-	var light = $Skeleton3D/flashlight_attachment/flashlight/Shoulder_light
+	var light = $Skeleton3D/flashlight_attachment/flashlight/flash_light
 	if light:
-		light.spot_angle = clamp(light.spot_angle + step, 15, 30)  # Adjust the clamp values as needed
+		light.spot_angle = clamp(light.spot_angle + step, 15, 40)  # Adjust the clamp values as needed
 		
 func adjust_spotlight_power(step: float) -> void:
-	var light = $Skeleton3D/flashlight_attachment/flashlight/Shoulder_light
+	var light = $Skeleton3D/flashlight_attachment/flashlight/flash_light
 	if light:
-		light.spot_range = clamp(light.spot_range + step, 15, 30)  # Adjust the clamp values as needed
+		light.spot_range = clamp(light.spot_range + step, 54, 162)  # Adjust the clamp values as needed
